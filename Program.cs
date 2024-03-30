df780a49e98ba58721381a3cd3fca4908e25832d
Alexey R <amareevich@gmail.com>
2024-03-30 13:09:25 +0300

    добавил первый цикл для получения кол-ва элементов в новом массиве

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿String [] ArrayStr = ["новый", "массив", "строк", "с", "1им", "или", "до", "3ех", "символов"];
+int count = 0;
+int retrunLen = 2;
+
+for (int i = 0; i <= ArrayStr.Length - 1; i++) 
+{
+    if (ArrayStr[i].Length - 1  <= retrunLen) 
+    {
+        count++;
+    }
+}
+
